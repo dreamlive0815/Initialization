58c40625cb853e43ffa7931b39124ebacc773da9
unknown <>
2018-04-19 23:08:26 +0800

    201804192308

--- Initialization/Stringifier.cs
+++ Initialization/Stringifier.cs
@@ -42,26 +42,31 @@ namespace Initialization
             IEnumerable<Section> sections = _ini.Sections;
             if(SectionOrder == SectionOrder.Name) sections = sections.OrderBy(i => i.Name);
             foreach(var section in sections) {
-
                 sb.Append('['); sb.Append(section.Name); sb.Append(']');
                 sb.Append(nl);
 
-                if(ParameterOrder == ParameterOrder.Key) {
+                if (ParameterOrder == ParameterOrder.Key) {
                     IEnumerable<Parameter> paramaters = section.Parameters;
                     paramaters = paramaters.OrderBy(i => i.Key);
-                    foreach(var paramater in paramaters) {
-
+                    foreach (var paramater in paramaters) {
+                        sb.Append(paramater.Key); sb.Append("="); sb.Append(paramater.Value);
+                        sb.Append(nl);
+                    }
+                } else {
+                    var list = section.Parameters.Cast<IPositionable>().ToList();
+                    if (_withComment) list.AddRange(section.Comments);
+                    var items = list.OrderBy(i => i.Line);
+                    foreach (var item in items) {
+                        sb.Append(item.ToString(_withComment));
+                        sb.Append(nl);
                     }
                 }
 
+                sb.Append(nl);
             }
-
 
             return sb.ToString();
         }
-
-
-
     }
 
     enum SectionOrder
